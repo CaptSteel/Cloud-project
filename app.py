d5ab537bb751e3fc8811ae0b680ffc0be83b1dd1
Sujeet <64351924+sujeet111@users.noreply.github.com>
2021-04-25 20:15:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,15 @@
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory
-from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-   if request.method == 'POST':
-       if 'file' not in request.files:
-           print('No file attached in request')
-           return redirect(request.url)
-       file = request.files['file']
-       if file.filename == '':
-           print('No file selected')
-           return redirect(request.url)
-       if file and allowed_file(file.filename):
-           filename = secure_filename(file.filename)
-           file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-           return redirect(url_for('uploaded_file', filename=filename))
-   return render_template('index.html')
-
-UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
-
 ALLOWED_EXTENSIONS = {'jpeg','png'}
 def allowed_file(filename):
    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+app.secret_key = b'secret'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+   if request.method == 'POST':
+       
+
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
+
