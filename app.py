85deccbbd6e3a2bd1ee420cb556fb430c71ec2a7
Sujeet <64351924+sujeet111@users.noreply.github.com>
2021-04-27 11:01:14 +0530

    .

--- app.py
+++ app.py
@@ -7,19 +7,20 @@ from PIL.ExifTags import TAGS
 app = Flask(__name__)  
 
 app.secret_key = "secret key" # for encrypting the session
+mylist = []
 
 #It will allow below 1MB contents only
-app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024 
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024 
 
 path = os.getcwd()
 # file Upload
-UPLOAD_FOLDER = os.path.join(path, 'uploads')
+UPLOAD_FOLDER = os.path.join(path, 'static\\uploads\\')
 
 if not os.path.isdir(UPLOAD_FOLDER):
     os.mkdir(UPLOAD_FOLDER)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS = set(['png','jpeg'])
+ALLOWED_EXTENSIONS = set(['png','jpeg', 'jpg'])
 def allowed_file(filename):
    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -41,17 +42,18 @@ def upload_file():
       if file and allowed_file(file.filename):
          filename = secure_filename(file.filename)
          file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) 
-         flash('http://127.0.0.1:5000/metainfo?filename='+filename)
+         flash('http://127.0.0.1:5000/metainfo/'+filename)
+         mylist.append('http://127.0.0.1:5000/metainfo/'+filename)
          return redirect('/')
       else:
          flash('Allowed file types are png and jpeg only!')
          return redirect(request.url)
  
-@app.route('/metainfo', methods=['GET','POST'])
-def metainfo():
+@app.route('/metainfo/<filename>', methods=['GET','POST'])
+def metainfo(filename):
    metadict={}
-   image_name = request.args.get('filename')
-   image_name = "uploads\{}".format(image_name)
+   image_name = UPLOAD_FOLDER + filename
+   filedir = 'uploads\\'+ filename
    image = Image.open(image_name)# read the image data using PIL
    exifdata = image.getexif() # extract EXIF data
    for tag_id in exifdata:
@@ -65,7 +67,15 @@ def metainfo():
          metadict = {"DateTimeOriginal":data}
       if  'OriginalRawFileName' in tag:
          metadict = {"OriginalRawFileName" : data}
-   return render_template('metainfo.html',metadict = metadict)
-  
+      if  'OriginalRawFileData' in tag:
+         metadict = {"OriginalRawFileData" : data}
+   return render_template('metainfo.html',metadict = metadict, image_name = image_name, filedir = filedir)
+
+@app.route('/mylinks', methods=['GET','POST'])
+def mylist():
+   return render_template('mylinks.html', mylist = mylist)
+
+
+
 if __name__ == '__main__':  
     app.run(debug = True)  
